abbd73a0116c0de059b6025ca1bda3abd199014a
Joábio Vilela <joabio.vilela@gmail.com>
2024-06-24 18:31:32 -0300

    feat: :lipstick: Aumentar tamanho das fontes das letras ao aplicar <h2>.

--- src/components/Missao.js
+++ src/components/Missao.js
@@ -4,9 +4,9 @@ function Missao() {
     return(
             <Container className="missao">
 
-                <Row><Col className='center'><h2>Nossa missão</h2><br/></Col></Row>
-                <Row><Col className='center'>Ajudamos os empreendedores a se prepararem para abrir suas empresas, alterar, e dar baixa.
-                    E também oferecemos soluções de contabilidade.</Col>
+                <Row><Col className='center'><h1>Nossa missão</h1><br/></Col></Row>
+                <Row><Col className='center'><h2>Ajudamos os empreendedores a se prepararem para abrir suas empresas, alterar, e dar baixa.
+                    E também oferecemos soluções de contabilidade.</h2></Col>
                 </Row>
             </Container>
     );
