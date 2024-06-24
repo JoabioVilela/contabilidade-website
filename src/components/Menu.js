5696d1b2626545f5b59085643a5ef0e8340ef86b
Joábio Vilela <joabio.vilela@gmail.com>
2024-06-24 18:37:07 -0300

    feat: :lipstick: Aumentar tamanho da fonte das letras do menu.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,8 +12,8 @@ function Menu() {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
            <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#servicos">Serviços</Nav.Link>
-            <Nav.Link href="#contato">Contato</Nav.Link>
+            <Nav.Link href="#servicos"><h2>Serviços</h2></Nav.Link>
+            <Nav.Link href="#contato"><h2>Contato</h2></Nav.Link>
           </Nav>
           </Navbar.Collapse>
         </Container>
