724365b22aae647776b6d57be932e63bda49b85d
Joábio Vilela <joabio.vilela@gmail.com>
2024-07-09 05:59:00 -0300

    style: :lipstick: Alterar textos títulos seções para CAIXA ALTA.

--- src/components/Missao.js
+++ src/components/Missao.js
@@ -4,7 +4,7 @@ function Missao() {
     return(
             <Container className="missao">
 
-                <Row><Col className='center'><h1>Nossa missão</h1><br/></Col></Row>
+                <Row><Col className='center'><h1>MISSÃO</h1><br/></Col></Row>
                 <Row><Col className='center'><h2>Ajudamos os empreendedores a se prepararem para abrir suas empresas, alterar, e dar baixa.
                     E também oferecemos soluções de contabilidade.</h2></Col>
                 </Row>
